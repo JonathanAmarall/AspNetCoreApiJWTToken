bc8409af22f2ef629c29b8a5fbc294327eac0d0d
JonathanAmarall <jhouamaral95@gmail.com>
2020-10-13 21:48:01 -0300

    Criado migration inicial

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,8 +6,12 @@ namespace Backend.Data
     public class DataContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder _optionsBuilder) { }
 
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder _optionsBuilder) { }
+        public DataContext(DbContextOptions<DataContext> _options) : base(_options)
+        {
+        }
         protected override void OnModelCreating(ModelBuilder _builder)
         {
 
